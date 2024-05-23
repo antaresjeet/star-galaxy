3a201d0cf99eb588dabd4156d85b4900ba56abef
Jeet <104215002+antaresjeet@users.noreply.github.com>
2024-05-23 17:08:32 +0530

    added google-site-verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,6 +38,8 @@ export default function MyStarWarsApp({
         <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+
+        <meta name="google-site-verification" content="GDMvkcIIhoR4e5RwNW-KhKKgyRi1pmKPzUpyY4arvrc" />
       </Head>
       <div className='flex flex-col min-h-screen'>
         <Header />
