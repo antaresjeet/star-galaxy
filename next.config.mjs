60fb74095221cf9ed17258202102ec628edf6604
Jeet <104215002+antaresjeet@users.noreply.github.com>
2024-05-23 17:15:27 +0530

    added locale

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   env: {
     swapiEndPoint: 'https://swapi.dev/api/',
     starWarsOfficialWebsite: 'https://www.starwars.com/'
-  }
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US'
+  },
 };
 
 export default nextConfig;
